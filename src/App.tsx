3eb405622fb71141645202095517d1055b326276
Eric Toreki <etoreki@udel.edu>
2022-03-24 14:53:58 -0400

    Added starting text ot Intro

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">React: Hooks In A Nutshell</header>
       <h2>Intro/Background</h2>
-      <p className="App-body">Test</p>
+      <div className="App-body">
+      <p>Before delving further into React hooks we need to discuss some vocabulary. It is important to know that React is divided into Components, and each component has its own state. So some important vocabulary in regards to state are: </p>
+      <li>
+        <ul>Model: </ul>
+        <ul>View: </ul>
+        <ul>Controller: </ul>
+      </li>
+      </div>
       <h2>useState</h2>
       <h2>Hooks</h2>
       <h2>Examples and Some Fun</h2>
