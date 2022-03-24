6e2f2b284227fa83180352a13fe9c935772185c7
Maxwell Wang <maxwang@udel.edu>
2022-03-24 14:56:44 -0400

    first stage implement the state for react memes

---
+++ src/clickThroughMeme.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import React from "react";
+
+export function ClickThroughMeme(): JSX.Element{
+    const [meme, changeMemes] = useState<string>("");
+
+    return <div></div>;
+}
