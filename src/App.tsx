ce14d93d6ce38326528bab8cd157785020653004
Eric Toreki <etoreki@udel.edu>
2022-03-24 14:36:59 -0400

    Added headers and styline for main header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">React: Hooks In A Nutshell</header>
+      <h2>Intro/Background</h2>
+      <p>Test</p>
+      <h2>useState</h2>
+      <h2>Hooks</h2>
+      <h2>Examples and Some Fun</h2>
     </div>
   );
 }
