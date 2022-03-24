90424395a0fdc441415204982cc40d3f3a842936
Maxwell Wang <maxwang@udel.edu>
2022-03-24 15:12:31 -0400

    now has an array :)

--- src/clickThroughMeme.tsx
+++ src/clickThroughMeme.tsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import React from "react";
 
 export function ClickThroughMeme(): JSX.Element{
-    const [meme, changeMemes] = useState<string>("");
+    const listOfMemes = ["react-hooks", "state mutation", "titleScreen"]
+    const [meme, changeMemes] = useState<string>(listOfMemes[0]);
 
     return <div></div>;
 }
