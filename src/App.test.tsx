f02431d99ade36064f535924a7e9ecc27f30f911
mwang840 <maxwang@udel.edu>
2023-08-06 11:41:40 -0600

    grammar fixes and icon changes, hope this deploys

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders author link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(
+    /By Maxwell Wang, Eric Toreki and Joshua Strassle/i
+  );
+
+  expect(titleElement).toBeInTheDocument();
 });
